f1416386ac67d2f0351b38f1e4189ca4565c7f51
pknowledge <jamesblssings@gmail.com>
2024-09-27 05:22:50 +0300

    Adding arithmetic

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2; // 62
+
+let random = Math.floor(Math.random() * 100) + 1; // Random integer greater than 0
+
+let num3 = 16;
+let num4 = 6;
+let mod = num3 % num4; // 4
+
+let max = Math.max(1, 5, 10, 15, 20); // 20
